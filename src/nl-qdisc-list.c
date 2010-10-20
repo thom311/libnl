a670ee5d72cc6c2492108edfe131f7547081748f
Thomas Graf <tgraf@suug.ch>
2010-10-20 15:26:06 +0200

    make nl-qdisc-list installable
    Also improves usability of nl-qdisc-list

--- src/nl-qdisc-list.c
+++ src/nl-qdisc-list.c
@@ -1,35 +1,38 @@
 /*
- * src/nl-qdisc-list.c     List Qdiscs
+ * src/nl-qdisc-list.c     List Queueing Disciplines
  *
  *	This library is free software; you can redistribute it and/or
  *	modify it under the terms of the GNU Lesser General Public
  *	License as published by the Free Software Foundation version 2.1
  *	of the License.
  *
- * Copyright (c) 2003-2009 Thomas Graf <tgraf@suug.ch>
+ * Copyright (c) 2003-2010 Thomas Graf <tgraf@suug.ch>
  */
 
 #include <netlink/cli/utils.h>
 #include <netlink/cli/qdisc.h>
 #include <netlink/cli/link.h>
 
-static int quiet = 0;
-
 static void print_usage(void)
 {
 	printf(
 	"Usage: nl-qdisc-list [OPTION]... [QDISC]\n"
 	"\n"
-	"Options\n"
-	" -f, --format=TYPE     Output format { brief | details | stats }\n"
+	"OPTIONS\n"
+	"     --details         Show details\n"
+	"     --stats           Show statistics\n"
 	" -h, --help            Show this help\n"
 	" -v, --version         Show versioning information\n"
 	"\n"
-	"QDisc Options\n"
-	" -d, --dev=DEV         Device the qdisc is attached to\n"
-	" -p, --parent=HANDLE   Identifier of parent qdisc\n"
-	" -H, --handle=HANDLE   Identifier\n"
+	" -d, --dev=DEV         Device the qdisc is attached to. (default: all)\n"
+	" -p, --parent=ID       Identifier of parent qdisc.\n"
+	" -i, --id=ID           Identifier.\n"
 	" -k, --kind=NAME       Kind of qdisc (e.g. pfifo_fast)\n"
+	"\n"
+	"EXAMPLE\n"
+	"    # Display statistics of all qdiscs attached to eth0\n"
+	"    $ nl-qdisc-list --details --dev=eth0\n"
+	"\n"
 	);
 	exit(0);
 }
@@ -53,33 +56,33 @@ int main(int argc, char *argv[])
 	for (;;) {
 		int c, optidx = 0;
 		enum {
-			ARG_YES = 257,
+			ARG_DETAILS = 257,
+			ARG_STATS = 258,
 		};
 		static struct option long_opts[] = {
-			{ "format", 1, 0, 'f' },
-			{ "quiet", 0, 0, 'q' },
+			{ "details", 0, 0, ARG_DETAILS },
+			{ "stats", 0, 0, ARG_STATS },
 			{ "help", 0, 0, 'h' },
 			{ "version", 0, 0, 'v' },
 			{ "dev", 1, 0, 'd' },
 			{ "parent", 1, 0, 'p' },
-			{ "handle", 1, 0, 'H' },
+			{ "id", 1, 0, 'i' },
 			{ "kind", 1, 0, 'k' },
 			{ 0, 0, 0, 0 }
 		};
 	
-		c = getopt_long(argc, argv, "f:qhvd:p:H:k:",
-				long_opts, &optidx);
+		c = getopt_long(argc, argv, "hvd:p:i:k:", long_opts, &optidx);
 		if (c == -1)
 			break;
 
 		switch (c) {
-		case 'f': params.dp_type = nl_cli_parse_dumptype(optarg); break;
-		case 'q': quiet = 1; break;
+		case ARG_DETAILS: params.dp_type = NL_DUMP_DETAILS; break;
+		case ARG_STATS: params.dp_type = NL_DUMP_STATS; break;
 		case 'h': print_usage(); break;
 		case 'v': nl_cli_print_version(); break;
 		case 'd': nl_cli_qdisc_parse_dev(qdisc, link_cache, optarg); break;
 		case 'p': nl_cli_qdisc_parse_parent(qdisc, optarg); break;
-		case 'H': nl_cli_qdisc_parse_handle(qdisc, optarg); break;
+		case 'i': nl_cli_qdisc_parse_handle(qdisc, optarg); break;
 		case 'k': nl_cli_qdisc_parse_kind(qdisc, optarg); break;
 		}
  	}

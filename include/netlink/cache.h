38db636f7877c7643463e51178e91695485eda27
Daniel Walter <sahne@0x90.at>
2011-03-22 10:53:51 +0100

    add missing nl_cache_search in cache.h
    fix missing declaration of nl_cache_search

--- include/netlink/cache.h
+++ include/netlink/cache.h
@@ -68,6 +68,8 @@ extern int			nl_cache_include(struct nl_cache *,
 
 /* General */
 extern int			nl_cache_is_empty(struct nl_cache *);
+extern struct nl_object *	nl_cache_search(struct nl_cache *,
+						struct nl_object *);
 extern void			nl_cache_mark_all(struct nl_cache *);
 
 /* Dumping */

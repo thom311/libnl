9426d03e3ae4510e3febf73fad520be37de05d7f
Stephane Fillod <fillods@users.sf.net>
2012-08-01 22:29:47 +0200

    tbf: fix false missing attr
    Fix false missing attribute in tbf_msg_fill() when applying a tbf qdisc.

--- lib/route/qdisc/tbf.c
+++ lib/route/qdisc/tbf.c
@@ -135,7 +135,7 @@ static int tbf_msg_fill(struct rtnl_tc *tc, void *data, struct nl_msg *msg)
 	struct rtnl_tbf *tbf = data;
 	int required = TBF_ATTR_RATE | TBF_ATTR_LIMIT;
 
-	if (!(tbf->qt_mask & required) != required)
+	if ((tbf->qt_mask & required) != required)
 		return -NLE_MISSING_ATTR;
 
 	memset(&opts, 0, sizeof(opts));

thom311/libnl

commit 2bdcde7e8e8bb78b165f093f1a708134f417e557

Fix types-related warnings based on clang diagnostics
1. Fix some places where unsigned value compared < 0
2. Fix obsolete %Z specifier to more portable %z
3. Some erroneous types substitution
4. nl_msec2str() - 64-bit msec is now properly used,

Only safe changes. I mean int <--> uint32_t and signed/unsigned fixes.
Some functinos require size_t argument instead of int, but changes of
signatures of that functions is terrible thing.

Also, I do not pretend for a full list of fixes.
Just to shut up clang -Wall -Wextra

One more thing. ifindex. I don't change that because changes will
be too big for simple fix.

src/lib/route.c
@@ -198,11 +198,15 @@ void nl_cli_route_parse_table(struct rtnl_route *route, char *arg)
 {
 	unsigned long lval;
 	char *endptr;
+	int table;
 
 	lval = strtoul(arg, &endptr, 0);
 	if (endptr == arg) {
-		if ((lval = rtnl_route_str2table(arg)) < 0)
+		if ((table = rtnl_route_str2table(arg)) < 0)
 			nl_cli_fatal(EINVAL, "Unknown table name \"%s\"", arg);
+	}
+	else {
+		table = lval;
 	}
 
 	rtnl_route_set_table(route, lval);
@@ -233,16 +237,20 @@ void nl_cli_route_parse_protocol(struct rtnl_route *route, char *arg)
 {
 	unsigned long lval;
 	char *endptr;
+	int proto;
 
 	lval = strtoul(arg, &endptr, 0);
 	if (endptr == arg) {
-		if ((lval = rtnl_route_str2proto(arg)) < 0)
+		if ((proto = rtnl_route_str2proto(arg)) < 0)
 			nl_cli_fatal(EINVAL,
 				     "Unknown routing protocol name \"%s\"",
 				     arg);
 	}
+	else {
+		proto = lval;
+	}
 
-	rtnl_route_set_protocol(route, lval);
+	rtnl_route_set_protocol(route, proto);
 }
 
 void nl_cli_route_parse_type(struct rtnl_route *route, char *arg)

lib/addr.c
@@ -368,7 +368,7 @@ int nl_addr_parse(const char *addrstr, int hint, struct nl_addr **result)
 	}
 
 	if (hint == AF_UNSPEC && strchr(str, ':')) {
-		int i = 0;
+		size_t i = 0;
 		char *s = str, *p;
 		for (;;) {
 			long l = strtol(s, &p, 16);
@@ -542,7 +542,7 @@ int nl_addr_cmp_prefix(struct nl_addr *a, struct nl_addr *b)
  */
 int nl_addr_iszero(struct nl_addr *addr)
 {
-	int i;
+	unsigned int i;
 
 	for (i = 0; i < addr->a_len; i++)
 		if (addr->a_addr[i])
@@ -823,7 +823,7 @@ unsigned int nl_addr_get_prefixlen(struct nl_addr *addr)
  */
 char *nl_addr2str(struct nl_addr *addr, char *buf, size_t size)
 {
-	int i;
+	unsigned int i;
 	char tmp[16];
 
 	if (!addr || !addr->a_len) {

lib/attr.c
@@ -188,7 +188,8 @@ static int validate_nla(struct nlattr *nla, int maxtype,
 			struct nla_policy *policy)
 {
 	struct nla_policy *pt;
-	int minlen = 0, type = nla_type(nla);
+	unsigned int minlen = 0;
+	int type = nla_type(nla);
 
 	if (type <= 0 || type > maxtype)
 		return 0;

lib/data.c
@@ -110,9 +110,6 @@ struct nl_data *nl_data_clone(struct nl_data *src)
  */
 int nl_data_append(struct nl_data *data, void *buf, size_t size)
 {
-	if (size < 0)
-		BUG();
-
 	if (size > 0) {
 		data->d_data = realloc(data->d_data, data->d_size + size);
 		if (!data->d_data)

lib/genl/genl.c
@@ -223,9 +223,9 @@ struct genlmsghdr *genlmsg_hdr(struct nlmsghdr *nlh)
  */
 int genlmsg_len(const struct genlmsghdr *gnlh)
 {
-	struct nlmsghdr *nlh;
+	const struct nlmsghdr *nlh;
 
-	nlh = (struct nlmsghdr *)((unsigned char *) gnlh - NLMSG_HDRLEN);
+	nlh = (const struct nlmsghdr *)((const unsigned char *) gnlh - NLMSG_HDRLEN);
 	return (nlh->nlmsg_len - GENL_HDRLEN - NLMSG_HDRLEN);
 }
 

lib/msg.c
@@ -178,7 +178,7 @@ int nlmsg_valid_hdr(const struct nlmsghdr *nlh, int hdrlen)
  */
 int nlmsg_ok(const struct nlmsghdr *nlh, int remaining)
 {
-	return (remaining >= (int)sizeof(struct nlmsghdr) &&
+	return (remaining >= sizeof(struct nlmsghdr) &&
 		nlh->nlmsg_len >= sizeof(struct nlmsghdr) &&
 		nlh->nlmsg_len <= remaining);
 }
@@ -867,7 +867,7 @@ void nl_msg_dump(struct nl_msg *msg, FILE *ofd)
 	"--------------------------   BEGIN NETLINK MESSAGE "
 	"---------------------------\n");
 
-	fprintf(ofd, "  [HEADER] %Zu octets\n", sizeof(struct nlmsghdr));
+	fprintf(ofd, "  [HEADER] %zu octets\n", sizeof(struct nlmsghdr));
 	print_hdr(ofd, msg);
 
 	if (hdr->nlmsg_type == NLMSG_ERROR &&
@@ -875,10 +875,10 @@ void nl_msg_dump(struct nl_msg *msg, FILE *ofd)
 		struct nl_msg *errmsg;
 		struct nlmsgerr *err = nlmsg_data(hdr);
 
-		fprintf(ofd, "  [ERRORMSG] %Zu octets\n", sizeof(*err));
+		fprintf(ofd, "  [ERRORMSG] %zu octets\n", sizeof(*err));
 		fprintf(ofd, "    .error = %d \"%s\"\n", err->error,
 			strerror(-err->error));
-		fprintf(ofd, "  [ORIGINAL MESSAGE] %Zu octets\n", sizeof(*hdr));
+		fprintf(ofd, "  [ORIGINAL MESSAGE] %zu octets\n", sizeof(*hdr));
 
 		errmsg = nlmsg_inherit(&err->msg);
 		print_hdr(ofd, errmsg);

lib/nl.c
@@ -417,7 +417,7 @@ errout:
 int nl_recv(struct nl_sock *sk, struct sockaddr_nl *nla,
 	    unsigned char **buf, struct ucred **creds)
 {
-	int n;
+	ssize_t n;
 	int flags = 0;
 	static int page_size = 0;
 	struct iovec iov;

lib/object.c
@@ -285,14 +285,14 @@ void nl_object_dump_buf(struct nl_object *obj, char *buf, size_t len)
 int nl_object_identical(struct nl_object *a, struct nl_object *b)
 {
 	struct nl_object_ops *ops = obj_ops(a);
-	int req_attrs;
+	uint32_t req_attrs;
 
 	/* Both objects must be of same type */
 	if (ops != obj_ops(b))
 		return 0;
 
 	req_attrs = ops->oo_id_attrs;
-	if (req_attrs == ~0)
+	if (req_attrs == 0xFFFFFFFF)
 		req_attrs = a->ce_mask & b->ce_mask;
 
 	/* Both objects must provide all required attributes to uniquely

commit 052a13119353f97e70216808b02bc692bcb18378

nl_cli_route_parse_table fixed typo in code
Bug introduced in 2bdcde7e8e8bb78b165f093f1a708134f417e557

src/lib/route.c
@@ -209,7 +209,7 @@ void nl_cli_route_parse_table(struct rtnl_route *route, char *arg)
 		table = lval;
 	}
 
-	rtnl_route_set_table(route, lval);
+	rtnl_route_set_table(route, table);
 }
 
 void nl_cli_route_parse_prio(struct rtnl_route *route, char *arg)
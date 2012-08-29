8cd2f5728a106931a1dd5ba96fb5a3d08da36362
Коренберг Марк (дома) <socketpair@gmail.com>
2012-08-30 00:15:12 +0600

    Fix typo in textual description in ct_dump_stats()
    Bug introduced in a0f1c0e281ee78ab8ee874bbb6c2140c12101284

--- lib/netfilter/ct_obj.c
+++ lib/netfilter/ct_obj.c
@@ -267,8 +267,8 @@ static void ct_dump_stats(struct nl_object *a, struct nl_dump_params *p)
 	    !nfnl_ct_test_bytes(ct, 1) ||
 	    !nfnl_ct_test_packets(ct, 1))
 	    {
-		nl_dump_line(p, "    Statics are not available.\n");
-		nl_dump_line(p, "    Please set sysctl net.netfilter.nf_conntrack_acct = 1\n");
+		nl_dump_line(p, "    Statistics are not available.\n");
+		nl_dump_line(p, "    Please set sysctl net.netfilter.nf_conntrack_acct=1\n");
 		nl_dump_line(p, "    (Require kernel 2.6.27)\n");
 		return;
 	    }

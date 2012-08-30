thom311/libnl

commit 794ac78c5618ee81a45e4f58694ee27b3403ebd7

nl_addr_cmp(): handle prefix length during address comparison
Signed-off-by: Brett Ciphery <brett.ciphery@windriver.com>

lib/addr.c
@@ -497,7 +497,10 @@ int nl_addr_cmp(struct nl_addr *a, struct nl_addr *b)
 		d = a->a_len - b->a_len;
 
 		if (a->a_len && d == 0)
-			return memcmp(a->a_addr, b->a_addr, a->a_len);
+			d = memcmp(a->a_addr, b->a_addr, a->a_len);
+
+			if (d == 0)
+				return (a->a_prefixlen - b->a_prefixlen);
 	}
 
 	return d;

commit 00261259f02b41b0689c87525fd5b6039b651c37

Address comparison bug fixed
Bug introduced in 794ac78c5618ee81a45e4f58694ee27b3403ebd7

lib/addr.c
@@ -497,11 +497,12 @@ int nl_addr_cmp(struct nl_addr *a, struct nl_addr *b)
 	if (d == 0) {
 		d = a->a_len - b->a_len;
 
-		if (a->a_len && d == 0)
+		if (a->a_len && d == 0) {
 			d = memcmp(a->a_addr, b->a_addr, a->a_len);
 
 			if (d == 0)
 				return (a->a_prefixlen - b->a_prefixlen);
+		}
 	}
 
 	return d;
8b8e26b33304c56370096cecbe58fdb3ede318c2
Thomas Graf <tgraf@suug.ch>
2013-02-05 13:03:02 +0100

    link: Fix af_ops leak on ENOMEM
    Signed-off-by: Thomas Graf <tgraf@suug.ch>

--- lib/route/link.c
+++ lib/route/link.c
@@ -72,8 +72,10 @@ static struct rtnl_link_af_ops *af_lookup_and_alloc(struct rtnl_link *link,
 	if (!af_ops)
 		return NULL;
 
-	if (!(data = rtnl_link_af_alloc(link, af_ops)))
+	if (!(data = rtnl_link_af_alloc(link, af_ops))) {
+		rtnl_link_af_ops_put(af_ops);
 		return NULL;
+	}
 
 	return af_ops;
 }

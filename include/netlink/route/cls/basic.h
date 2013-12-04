c855e4fef5ecf7e0723d65b6b16395eff5e7c097
Cong Wang <xiyou.wangcong@gmail.com>
2013-12-03 20:16:10 -0800

    add declaration for rtnl_basic_add_action()
    Signed-off-by: Cong Wang <xiyou.wangcong@gmail.com>
    Signed-off-by: Thomas Graf <tgraf@suug.ch>

--- include/netlink/route/cls/basic.h
+++ include/netlink/route/cls/basic.h
@@ -15,6 +15,7 @@
 #include <netlink/netlink.h>
 #include <netlink/route/classifier.h>
 #include <netlink/route/cls/ematch.h>
+#include <netlink/route/action.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -25,6 +26,7 @@ extern uint32_t			rtnl_basic_get_target(struct rtnl_cls *);
 extern void			rtnl_basic_set_ematch(struct rtnl_cls *,
 						      struct rtnl_ematch_tree *);
 extern struct rtnl_ematch_tree *rtnl_basic_get_ematch(struct rtnl_cls *);
+extern int rtnl_basic_add_action(struct rtnl_cls *, struct rtnl_act *);
 extern int rtnl_basic_del_action(struct rtnl_cls *, struct rtnl_act *);
 
 #ifdef __cplusplus

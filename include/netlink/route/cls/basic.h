a858d996e952af5bf449b87775f74355d492a0ec
Thomas Graf <tgraf@suug.ch>
2013-12-09 15:43:33 +0100

    basic: Declare rtnl_act in basic.h to silence warning
    Signed-off-by: Thomas Graf <tgraf@suug.ch>

--- include/netlink/route/cls/basic.h
+++ include/netlink/route/cls/basic.h
@@ -6,7 +6,7 @@
  *	License as published by the Free Software Foundation version 2.1
  *	of the License.
  *
- * Copyright (c) 2008-2010 Thomas Graf <tgraf@suug.ch>
+ * Copyright (c) 2008-2013 Thomas Graf <tgraf@suug.ch>
  */
 
 #ifndef NETLINK_BASIC_H_
@@ -19,6 +19,8 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+struct rtnl_act;
 
 extern void			rtnl_basic_set_target(struct rtnl_cls *, uint32_t);
 extern uint32_t			rtnl_basic_get_target(struct rtnl_cls *);

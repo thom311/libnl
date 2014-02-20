thom311/libnl

commit b3fb89f445108677d405c62865b25aeea209d10a

Handle the case where nl_msec2str is passed 0 msecs

lib/utils.c
@@ -559,6 +559,11 @@ char * nl_msec2str(uint64_t msec, char *buf, size_t len)
 #undef  _SPLIT
 	split[4] = msec;
 
+	if (msec == 0) {
+		snprintf(buf, len, "0msec");
+		return buf_orig;
+	}
+
 	for (i = 0; i < ARRAY_SIZE(split) && len; i++) {
 		int l;
 		if (split[i] == 0)

commit 3fb0aae0bc37eafe868d28f0f12ee8803d7ad266

utils: fix nl_msec2str() which always returned '0msec' for whole second durations
If the duration was without subsecond part, the function always returned
'0msec', instead of giving the time in days, hours, minutes or seconds.

Regression introduced by commit b3fb89f445108677d405c62865b25aeea209d10a.

Signed-off-by: Thomas Haller <thaller@redhat.com>
Acked-by: Thomas Graf <tgraf@suug.ch>

lib/utils.c
@@ -551,6 +551,11 @@ char * nl_msec2str(uint64_t msec, char *buf, size_t len)
 	static const char *units[5] = {"d", "h", "m", "s", "msec"};
 	char * const buf_orig = buf;
 
+	if (msec == 0) {
+		snprintf(buf, len, "0msec");
+		return buf_orig;
+	}
+
 #define _SPLIT(idx, unit) if ((split[idx] = msec / unit)) msec %= unit
 	_SPLIT(0, 86400000);	/* days */
 	_SPLIT(1, 3600000);	/* hours */
@@ -558,11 +563,6 @@ char * nl_msec2str(uint64_t msec, char *buf, size_t len)
 	_SPLIT(3, 1000);	/* seconds */
 #undef  _SPLIT
 	split[4] = msec;
-
-	if (msec == 0) {
-		snprintf(buf, len, "0msec");
-		return buf_orig;
-	}
 
 	for (i = 0; i < ARRAY_SIZE(split) && len; i++) {
 		int l;
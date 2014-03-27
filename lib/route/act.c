baa2cadd02086a053eb140c91d7f0681112ac370
Cong Wang <xiyou.wangcong@gmail.com>
2014-03-26 22:43:16 -0700

    route: fix off-by-one in rtnl_act_parse()
    Signed-off-by: Cong Wang <xiyou.wangcong@gmail.com>
    Acked-by: Thomas Graf <tgraf@suug.ch>
    Signed-off-by: Thomas Haller <thaller@redhat.com>

--- lib/route/act.c
+++ lib/route/act.c
@@ -390,7 +390,7 @@ int rtnl_act_parse(struct rtnl_act **head, struct nlattr *tb)
 	if (err < 0)
 		return err;
 
-	for (i = 1; i <= TCA_ACT_MAX_PRIO && nla[i]; i++) {
+	for (i = 0; i < TCA_ACT_MAX_PRIO && nla[i]; i++) {
 		struct rtnl_act *act;
 		struct rtnl_tc *tc;
 

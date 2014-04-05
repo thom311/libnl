690545a20eb486d4829900378caa1dc4dd5aa0ba
Cong Wang <xiyou.wangcong@gmail.com>
2014-04-04 18:15:16 -0700

    act: fix policy range check
    mirred action should accept all TC_ACT* policy
    
    Signed-off-by: Cong Wang <xiyou.wangcong@gmail.com>
    Acked-by: Thomas Graf <tgraf@suug.ch>
    Signed-off-by: Thomas Haller <thaller@redhat.com>

--- lib/route/act/mirred.c
+++ lib/route/act/mirred.c
@@ -187,7 +187,7 @@ int rtnl_mirred_set_policy(struct rtnl_act *act, int policy)
 	if (!(u = (struct rtnl_mirred *) rtnl_tc_data(TC_CAST(act))))
 		return -NLE_NOMEM;
 
-	if (policy > TC_POLICE_PIPE || policy < TC_POLICE_OK)
+	if (policy > TC_ACT_REPEAT || policy < TC_ACT_OK)
 		return -NLE_INVAL;
 
 	switch (u->m_parm.eaction) {

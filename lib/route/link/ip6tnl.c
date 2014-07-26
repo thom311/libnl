2592a852fbeccfe723e98df31536bd2e4771227a
Thomas Graf <tgraf@suug.ch>
2014-07-26 21:33:31 +0200

    link/ip6tnl: Include <netlink/route/link/ip6tnl.h>
    Signed-off-by: Thomas Graf <tgraf@suug.ch>
    Signed-off-by: Thomas Haller <thaller@redhat.com>

--- lib/route/link/ip6tnl.c
+++ lib/route/link/ip6tnl.c
@@ -28,6 +28,7 @@
 #include <netlink/utils.h>
 #include <netlink/object.h>
 #include <netlink/route/rtnl.h>
+#include <netlink/route/link/ip6tnl.h>
 #include <netlink-private/route/link/api.h>
 #include <linux/if_tunnel.h>
 #include <netinet/in.h>

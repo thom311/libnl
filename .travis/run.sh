1e5dd2d886f68e8f598db0fb8ca46b614e1bf28f
Thomas Haller <thaller@redhat.com>
2018-01-16 08:35:54 +0100

    travis: enable more warnings during build

--- .travis/run.sh
+++ .travis/run.sh
@@ -2,13 +2,14 @@
 
 set -e
 
-CFLAGS="-Werror"
+CFLAGS="-Werror -Wall -Wdeclaration-after-statement"
 
 if [ "$CC" = "clang" ]; then
-	CFLAGS="$CFLAGS -Wno-error=unused-command-line-argument"
+	CFLAGS="$CFLAGS -Wno-error=unused-command-line-argument -Wno-error=unused-function"
 fi
 
+export CFLAGS
 ./autogen.sh
 ./configure
-make CFLAGS="$CFLAGS"
-make CFLAGS="$CFLAGS" check
+make -j 5
+make -j 5 check

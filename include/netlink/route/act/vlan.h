8f75047868c2b443ccaf6086ca3338a6ac624391
Volodymyr Bendiuga <volodymyr.bendiuga@westermo.se>
2018-05-04 11:13:10 +0200

    route:act: add vlan action
    For more information please see:
    http://man7.org/linux/man-pages/man8/tc-vlan.8.html
    
    Signed-off-by: Volodymyr Bendiuga <volodymyr.bendiuga@westermo.se>

---
+++ include/netlink/route/act/vlan.h
@@ -0,0 +1,39 @@
+/*
+ * netlink/route/act/vlan.h	vlan action
+ *
+ *	This library is free software; you can redistribute it and/or
+ *	modify it under the terms of the GNU Lesser General Public
+ *	License as published by the Free Software Foundation version 2.1
+ *	of the License.
+ *
+ * Copyright (c) 2018 Volodymyr Bendiuga <volodymyr.bendiuga@gmail.com>
+ */
+
+#ifndef NETLINK_VLAN_H_
+#define NETLINK_VLAN_H_
+
+#include <netlink/netlink.h>
+#include <netlink/cache.h>
+#include <netlink/route/action.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern int rtnl_vlan_set_mode(struct rtnl_act *act, int mode);
+extern int rtnl_vlan_get_mode(struct rtnl_act *act);
+extern int rtnl_vlan_set_action(struct rtnl_act *act, int action);
+extern int rtnl_vlan_get_action(struct rtnl_act *act);
+extern int rtnl_vlan_set_protocol(struct rtnl_act *act, uint16_t protocol);
+extern uint16_t rtnl_vlan_get_protocol(struct rtnl_act *act);
+extern int rtnl_vlan_set_vlan_id(struct rtnl_act *act, uint16_t vid);
+extern uint16_t rtnl_vlan_get_vlan_id(struct rtnl_act *act);
+extern int rtnl_vlan_set_vlan_prio(struct rtnl_act *act, uint8_t prio);
+extern uint8_t rtnl_vlan_get_vlan_prio(struct rtnl_act *act);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif	/* NETLINK_VLAN_H */

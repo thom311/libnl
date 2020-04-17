56114876b236430afa6f86fff90ede15e0c52914
Thomas Haller <thaller@redhat.com>
2020-04-17 18:53:34 +0200

    lib/trivial: whitespace

--- lib/hashtable.c
+++ lib/hashtable.c
@@ -53,15 +53,15 @@ void nl_hash_table_free(nl_hash_table_t *ht)
 	int i;
 
 	for(i = 0; i < ht->size; i++) {
-	    nl_hash_node_t *node = ht->nodes[i];
-	    nl_hash_node_t *saved_node;
+		nl_hash_node_t *node = ht->nodes[i];
+		nl_hash_node_t *saved_node;
 
-	    while (node) {
-		   saved_node = node;
-		   node = node->next;
-		   nl_object_put(saved_node->obj);
-		   free(saved_node);
-	    }
+		while (node) {
+			saved_node = node;
+			node = node->next;
+			nl_object_put(saved_node->obj);
+			free(saved_node);
+		}
 	}
 
 	free(ht->nodes);
@@ -88,9 +88,9 @@ struct nl_object* nl_hash_table_lookup(nl_hash_table_t *ht,
 	node = ht->nodes[key_hash];
 
 	while (node) {
-	       if (nl_object_identical(node->obj, obj))
-		   return node->obj;
-	       node = node->next;
+		if (nl_object_identical(node->obj, obj))
+			return node->obj;
+		node = node->next;
 	}
 
 	return NULL;
@@ -118,11 +118,11 @@ int nl_hash_table_add(nl_hash_table_t *ht, struct nl_object *obj)
 	node = ht->nodes[key_hash];
 
 	while (node) {
-	       if (nl_object_identical(node->obj, obj)) {
-		   NL_DBG(2, "Warning: Add to hashtable found duplicate...\n");
-		   return -NLE_EXIST;
-	       }
-	       node = node->next;
+		if (nl_object_identical(node->obj, obj)) {
+			NL_DBG(2, "Warning: Add to hashtable found duplicate...\n");
+			return -NLE_EXIST;
+		}
+		node = node->next;
 	}
 
 	NL_DBG (5, "adding cache entry of obj %p in table %p, with hash 0x%x\n",

3d1fb006c85922907702c17909be5b2226bbe45c
Thomas Haller <thaller@redhat.com>
2022-03-03 15:22:13 +0100

    tests/check-addr: replace deprecated fail_if() macro from libcheck with ck_assert_msg()
    fail_if() is long deprecated. Worse, it triggers a "-Wformat-extra-args"
    warning due to a trailing NULL. See [1].
    
    [1] https://github.com/libcheck/check/commit/82540c5428d3818b64d6a8aefb601e722520651f

--- tests/check-addr.c
+++ tests/check-addr.c
@@ -14,33 +14,33 @@ START_TEST(addr_alloc)
 	struct nl_addr *addr;
 
 	addr = nl_addr_alloc(16);
-	fail_if(addr == NULL,
+	ck_assert_msg(addr,
 		"Allocation should not return NULL");
 
-	fail_if(nl_addr_iszero(addr) == 0,
+	ck_assert_msg(nl_addr_iszero(addr) != 0,
 		"New empty address should be all zeros");
 
-	fail_if(nl_addr_get_family(addr) != AF_UNSPEC,
+	ck_assert_msg(nl_addr_get_family(addr) == AF_UNSPEC,
 		"New empty address should have family AF_UNSPEC");
 
-	fail_if(nl_addr_get_prefixlen(addr) != 0,
+	ck_assert_msg(nl_addr_get_prefixlen(addr) == 0,
 		"New empty address should have prefix length 0");
 
-	fail_if(nl_addr_shared(addr),
+	ck_assert_msg(!nl_addr_shared(addr),
 		"New empty address should not be shared");
 
-	fail_if(nl_addr_get(addr) != addr,
+	ck_assert_msg(nl_addr_get(addr) == addr,
 		"nl_addr_get() should return pointer to address");
 
-	fail_if(nl_addr_shared(addr) == 0,
+	ck_assert_msg(nl_addr_shared(addr) != 0,
 		"Address should be shared after call to nl_addr_get()");
 
 	nl_addr_put(addr);
 
-	fail_if(nl_addr_shared(addr),
+	ck_assert_msg(!nl_addr_shared(addr),
 		"Address should not be shared after call to nl_addr_put()");
 
-	fail_if(nl_addr_fill_sockaddr(addr, NULL, 0) == 0,
+	ck_assert_msg(nl_addr_fill_sockaddr(addr, NULL, 0) != 0,
 		"Socket address filling should fail for empty address");
 
 	nl_addr_put(addr);
@@ -54,39 +54,39 @@ START_TEST(addr_binary_addr)
 	char baddr2[6] = { 0x1, 0x2, 0x3, 0x4, 0x5, 0x6 };
 
 	addr = nl_addr_alloc(4);
-	fail_if(addr == NULL,
+	ck_assert_msg(addr != NULL,
 		"Allocation should not return NULL");
 
-	fail_if(nl_addr_set_binary_addr(addr, baddr, 4) < 0,
+	ck_assert_msg(nl_addr_set_binary_addr(addr, baddr, 4) >= 0,
 		"Valid binary address should be settable");
 
-	fail_if(nl_addr_get_prefixlen(addr) != 0,
+	ck_assert_msg(nl_addr_get_prefixlen(addr) == 0,
 		"Prefix length should be unchanged after nl_addr_set_binary_addr()");
 
-	fail_if(nl_addr_get_len(addr) != 4,
+	ck_assert_msg(nl_addr_get_len(addr) == 4,
 		"Address length should be 4");
 
-	fail_if(nl_addr_set_binary_addr(addr, baddr2, 6) == 0,
+	ck_assert_msg(nl_addr_set_binary_addr(addr, baddr2, 6) != 0,
 		"Should not be able to set binary address exceeding maximum length");
 
-	fail_if(nl_addr_get_len(addr) != 4,
+	ck_assert_msg(nl_addr_get_len(addr) == 4,
 		"Address length should still be 4");
 
-	fail_if(nl_addr_guess_family(addr) != AF_INET,
+	ck_assert_msg(nl_addr_guess_family(addr) == AF_INET,
 		"Binary address of length 4 should be guessed as AF_INET");
 
-	fail_if(memcmp(baddr, nl_addr_get_binary_addr(addr), 4) != 0,
+	ck_assert_msg(memcmp(baddr, nl_addr_get_binary_addr(addr), 4) == 0,
 		"Binary address mismatches");
 
 	addr2 = nl_addr_build(AF_UNSPEC, baddr, 4);
-	fail_if(addr2 == NULL,
+	ck_assert_msg(addr2 != NULL,
 		"Building of address should not fail");
 
 	nl_addr_set_prefixlen(addr, 32);
-	fail_if(nl_addr_get_prefixlen(addr) != 32,
+	ck_assert_msg(nl_addr_get_prefixlen(addr) == 32,
 		"Prefix length should be successful changed after nl_addr_set_prefixlen()");
 
-	fail_if(nl_addr_cmp(addr, addr2),
+	ck_assert_msg(!nl_addr_cmp(addr, addr2),
 		"Addresses built from same binary address should match");
 
 	nl_addr_put(addr);
@@ -102,32 +102,32 @@ START_TEST(addr_parse4)
 	char *addr_str = "10.0.0.1/16";
 	char buf[128];
 
-	fail_if(nl_addr_parse(addr_str, AF_INET6, &addr4) == 0,
+	ck_assert_msg(nl_addr_parse(addr_str, AF_INET6, &addr4) != 0,
 		"Should not be able to parse IPv4 address in IPv6 mode");
 
-	fail_if(nl_addr_parse(addr_str, AF_UNSPEC, &addr4) != 0,
+	ck_assert_msg(nl_addr_parse(addr_str, AF_UNSPEC, &addr4) == 0,
 		"Should be able to parse \"%s\"", addr_str);
 
-	fail_if(nl_addr_get_family(addr4) != AF_INET,
+	ck_assert_msg(nl_addr_get_family(addr4) == AF_INET,
 		"Address family should be AF_INET");
 
-	fail_if(nl_addr_get_prefixlen(addr4) != 16,
+	ck_assert_msg(nl_addr_get_prefixlen(addr4) == 16,
 		"Prefix length should be 16");
 
-	fail_if(nl_addr_iszero(addr4),
+	ck_assert_msg(!nl_addr_iszero(addr4),
 		"Address should not be all zeroes");
 
 	clone = nl_addr_clone(addr4);
-	fail_if(clone == NULL,
+	ck_assert_msg(clone != NULL,
 		"Cloned address should not be NULL");
 
-	fail_if(nl_addr_cmp(addr4, clone) != 0,
+	ck_assert_msg(nl_addr_cmp(addr4, clone) == 0,
 		"Cloned address should not mismatch original");
 
-	fail_if(nl_addr_fill_sockaddr(addr4, (struct sockaddr *) &sin, &len) != 0,
+	ck_assert_msg(nl_addr_fill_sockaddr(addr4, (struct sockaddr *) &sin, &len) == 0,
 		"Should be able to fill socketaddr");
 
-	fail_if(strcmp(nl_addr2str(addr4, buf, sizeof(buf)), addr_str),
+	ck_assert_msg(!strcmp(nl_addr2str(addr4, buf, sizeof(buf)), addr_str),
 		"Address translated back to string does not match original");
 
 	nl_addr_put(addr4);
@@ -143,32 +143,32 @@ START_TEST(addr_parse6)
 	char *addr_str = "2001:1:2::3/64";
 	char buf[128];
 
-	fail_if(nl_addr_parse(addr_str, AF_INET, &addr6) == 0,
+	ck_assert_msg(nl_addr_parse(addr_str, AF_INET, &addr6) != 0,
 		"Should not be able to parse IPv6 address in IPv4 mode");
 
-	fail_if(nl_addr_parse(addr_str, AF_UNSPEC, &addr6) != 0,
+	ck_assert_msg(nl_addr_parse(addr_str, AF_UNSPEC, &addr6) == 0,
 		"Should be able to parse \"%s\"", addr_str);
 
-	fail_if(nl_addr_get_family(addr6) != AF_INET6,
+	ck_assert_msg(nl_addr_get_family(addr6) == AF_INET6,
 		"Address family should be AF_INET6");
 
-	fail_if(nl_addr_get_prefixlen(addr6) != 64,
+	ck_assert_msg(nl_addr_get_prefixlen(addr6) == 64,
 		"Prefix length should be 64");
 
-	fail_if(nl_addr_iszero(addr6),
+	ck_assert_msg(!nl_addr_iszero(addr6),
 		"Address should not be all zeroes");
 
 	clone = nl_addr_clone(addr6);
-	fail_if(clone == NULL,
+	ck_assert_msg(clone != NULL,
 		"Cloned address should not be NULL");
 
-	fail_if(nl_addr_cmp(addr6, clone) != 0,
+	ck_assert_msg(nl_addr_cmp(addr6, clone) == 0,
 		"Cloned address should not mismatch original");
 
-	fail_if(nl_addr_fill_sockaddr(addr6, (struct sockaddr *) &sin, &len) != 0,
+	ck_assert_msg(nl_addr_fill_sockaddr(addr6, (struct sockaddr *) &sin, &len) == 0,
 		"Should be able to fill socketaddr");
 
-	fail_if(strcmp(nl_addr2str(addr6, buf, sizeof(buf)), addr_str),
+	ck_assert_msg(!strcmp(nl_addr2str(addr6, buf, sizeof(buf)), addr_str),
 		"Address translated back to string does not match original");
 
 	nl_addr_put(addr6);
@@ -182,10 +182,10 @@ START_TEST(addr_info)
 	char *addr_str = "127.0.0.1";
 	struct addrinfo *result;
 
-	fail_if(nl_addr_parse(addr_str, AF_UNSPEC, &addr) != 0,
+	ck_assert_msg(nl_addr_parse(addr_str, AF_UNSPEC, &addr) == 0,
 		"Parsing of valid address should not fail");
 
-	fail_if(nl_addr_info(addr, &result) != 0,
+	ck_assert_msg(nl_addr_info(addr, &result) == 0,
 		"getaddrinfo() on loopback address should work");
 
 	freeaddrinfo(result);

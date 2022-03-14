92035e25e6207c8b2902f1bd931503c33d82ddaf
Thomas Haller <thaller@redhat.com>
2022-03-14 08:52:17 +0100

    route/mdb: cleanup mdb.h header
    - drop leading tabs. Yes, it's inside an extern C block, but so is all
      our code in public headers. That should not by default increase the
      indentation level.
    
    - drop unused rtnl_mdb_add().
    
    - forward declare "struct rtnl_mdb".

--- include/netlink/route/mdb.h
+++ include/netlink/route/mdb.h
@@ -11,32 +11,33 @@
 extern "C" {
 #endif
 
-	struct rtnl_mdb_entry;
-	struct rtnl_mdb *rtnl_mdb_alloc(void);
-	void rtnl_mdb_put(struct rtnl_mdb *mdb);
+struct rtnl_mdb;
+struct rtnl_mdb_entry;
 
-	int rtnl_mdb_alloc_cache(struct nl_sock *sk, struct nl_cache **result);
-	int rtnl_mdb_alloc_cache_flags(struct nl_sock *sock,
-				       struct nl_cache **result,
-				       unsigned int flags);
+struct rtnl_mdb *rtnl_mdb_alloc(void);
+void rtnl_mdb_put(struct rtnl_mdb *mdb);
 
-	int rtnl_mdb_add();
-	struct rtnl_mdb_entry *rtnl_mdb_entry_alloc(void);
+int rtnl_mdb_alloc_cache(struct nl_sock *sk, struct nl_cache **result);
+int rtnl_mdb_alloc_cache_flags(struct nl_sock *sock,
+			       struct nl_cache **result,
+			       unsigned int flags);
 
-	uint32_t rtnl_mdb_get_ifindex(struct rtnl_mdb *mdb);
-	void rtnl_mdb_add_entry(struct rtnl_mdb *mdb,
-				struct rtnl_mdb_entry *_entry);
+struct rtnl_mdb_entry *rtnl_mdb_entry_alloc(void);
 
-	void rtnl_mdb_foreach_entry(struct rtnl_mdb *mdb,
-				    void (*cb)(struct rtnl_mdb_entry *, void *),
-				    void *arg);
+uint32_t rtnl_mdb_get_ifindex(struct rtnl_mdb *mdb);
+void rtnl_mdb_add_entry(struct rtnl_mdb *mdb,
+			struct rtnl_mdb_entry *_entry);
 
-	int rtnl_mdb_entry_get_ifindex(struct rtnl_mdb_entry *mdb_entry);
-	int rtnl_mdb_entry_get_vid(struct rtnl_mdb_entry *mdb_entry);
-	int rtnl_mdb_entry_get_state(struct rtnl_mdb_entry *mdb_entry);
-	struct nl_addr *rtnl_mdb_entry_get_addr(struct rtnl_mdb_entry
-						*mdb_entry);
-	uint16_t rtnl_mdb_entry_get_proto(struct rtnl_mdb_entry *mdb_entry);
+void rtnl_mdb_foreach_entry(struct rtnl_mdb *mdb,
+			    void (*cb)(struct rtnl_mdb_entry *, void *),
+			    void *arg);
+
+int rtnl_mdb_entry_get_ifindex(struct rtnl_mdb_entry *mdb_entry);
+int rtnl_mdb_entry_get_vid(struct rtnl_mdb_entry *mdb_entry);
+int rtnl_mdb_entry_get_state(struct rtnl_mdb_entry *mdb_entry);
+struct nl_addr *rtnl_mdb_entry_get_addr(struct rtnl_mdb_entry
+					*mdb_entry);
+uint16_t rtnl_mdb_entry_get_proto(struct rtnl_mdb_entry *mdb_entry);
 #ifdef __cplusplus
 }
 #endif

thom311/libnl

commit a59cab6d0b0f58a4b58b3b0b5250a460e768bf0c

lib/route: SRIOV Clone Support
This patch adds support for cloning SRIOV VF specific data in the
link object.

Signed-off-by: Jef Oliver <jef.oliver@intel.com>
Signed-off-by: Thomas Haller <thaller@redhat.com>

lib/route/link/sriov.c
@@ -79,6 +79,58 @@ static struct nla_policy sriov_stats_policy[IFLA_VF_STATS_MAX+1] = {
 
 /** @endcond */
 
+/* Clone SRIOV VF list in link object */
+int rtnl_link_sriov_clone(struct rtnl_link *dst, struct rtnl_link *src) {
+	int err = 0;
+	struct nl_addr *vf_addr;
+	struct rtnl_link_vf *s_list, *d_vf, *s_vf, *next, *dest_h = NULL;
+	nl_vf_vlans_t *src_vlans = NULL, *dst_vlans = NULL;
+	nl_vf_vlan_info_t *src_vlan_info = NULL, *dst_vlan_info = NULL;
+
+	if (!(err = rtnl_link_has_vf_list(src)))
+		return 0;
+
+	dst->l_vf_list = rtnl_link_vf_alloc();
+	if (!dst->l_vf_list)
+		return -NLE_NOMEM;
+	dest_h = dst->l_vf_list;
+	s_list = src->l_vf_list;
+
+	nl_list_for_each_entry_safe(s_vf, next, &s_list->vf_list, vf_list) {
+		if (!(d_vf = rtnl_link_vf_alloc()))
+			return -NLE_NOMEM;
+
+		memcpy(d_vf, s_vf, sizeof(*s_vf));
+
+		if (s_vf->ce_mask & SRIOV_ATTR_ADDR) {
+			vf_addr = nl_addr_clone(s_vf->vf_lladdr);
+			if (!vf_addr)
+				return -NLE_NOMEM;
+			d_vf->vf_lladdr = vf_addr;
+		}
+
+		if (s_vf->ce_mask & SRIOV_ATTR_VLAN) {
+			src_vlans = s_vf->vf_vlans;
+			src_vlan_info = src_vlans->vlans;
+
+			err = rtnl_link_vf_vlan_alloc(&dst_vlans,
+						      src_vlans->size);
+			if (err < 0)
+				return err;
+			dst_vlan_info = dst_vlans->vlans;
+			memcpy(dst_vlans, src_vlans, sizeof(nl_vf_vlans_t));
+			memcpy(dst_vlan_info, src_vlan_info,
+			       dst_vlans->size * sizeof(dst_vlan_info));
+			d_vf->vf_vlans = dst_vlans;
+		}
+
+		nl_list_add_head(&d_vf->vf_list, &dest_h->vf_list);
+		dest_h = d_vf;
+	}
+
+	return 0;
+}
+
 /* Free stored SRIOV VF data */
 void rtnl_link_sriov_free_data(struct rtnl_link *link) {
 	int err = 0;

include/netlink-private/route/link/sriov.h
@@ -20,6 +20,7 @@
 extern "C" {
 #endif
 
+extern int rtnl_link_sriov_clone(struct rtnl_link *, struct rtnl_link *);
 extern void rtnl_link_sriov_free_data(struct rtnl_link *);
 extern int rtnl_link_sriov_parse_vflist(struct rtnl_link *, struct nlattr **);
 

lib/route/link.c
@@ -315,6 +315,10 @@ static int link_clone(struct nl_object *_dst, struct nl_object *_src)
 		if (!(dst->l_phys_port_id = nl_data_clone(src->l_phys_port_id)))
 			return -NLE_NOMEM;
 
+	if (src->ce_mask & LINK_ATTR_VF_LIST)
+		if ((err = rtnl_link_sriov_clone(dst, src)) < 0)
+			return err;
+
 	return 0;
 }
 

libnl-route-3.sym
@@ -987,6 +987,7 @@ global:
 	rtnl_link_has_vf_list;
 	rtnl_link_set_vf_list;
 	rtnl_link_unset_vf_list;
+	rtnl_link_sriov_clone;
 	rtnl_link_sriov_free_data;
 	rtnl_link_sriov_parse_vflist;
 	rtnl_link_vf_alloc;

commit bf3585ff248ab309f12a328687ac895f09bc0709

route/link/sriov: fix initializing vlans in rtnl_link_sriov_clone()
  Error: SIZEOF_MISMATCH (CWE-398):
  libnl-3.6.0/lib/route/link/sriov.c:125: suspicious_sizeof: Passing argument "dst_vlan_info" of type "nl_vf_vlan_info_t *" and argument "dst_vlans->size * 8UL /* sizeof (dst_vlan_info) */"
  libnl-3.6.0/lib/route/link/sriov.c:125: remediation: Did you intend to use "sizeof (*dst_vlan_info)" instead of "sizeof (dst_vlan_info)"?
  #  123|   			dst_vlan_info = dst_vlans->vlans;
  #  124|   			memcpy(dst_vlans, src_vlans, sizeof(nl_vf_vlans_t));
  #  125|-> 			memcpy(dst_vlan_info, src_vlan_info,
  #  126|   			       dst_vlans->size * sizeof(dst_vlan_info));
  #  127|   			d_vf->vf_vlans = dst_vlans;

Fixes: a59cab6d0b0f ('lib/route: SRIOV Clone Support')

lib/route/link/sriov.c
@@ -123,7 +123,7 @@ int rtnl_link_sriov_clone(struct rtnl_link *dst, struct rtnl_link *src) {
 			dst_vlan_info = dst_vlans->vlans;
 			memcpy(dst_vlans, src_vlans, sizeof(nl_vf_vlans_t));
 			memcpy(dst_vlan_info, src_vlan_info,
-			       dst_vlans->size * sizeof(dst_vlan_info));
+			       dst_vlans->size * sizeof(*dst_vlan_info));
 			d_vf->vf_vlans = dst_vlans;
 		}
 
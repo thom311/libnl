thom311/libnl

commit b6cc13d76b2927640dc0e41b98fff0f211c643d4

Supporting Hardware offload capability for MACsec
[thaller@redhat.com: adjust libnl-route-3.sym file]

https://github.com/thom311/libnl/pull/300

lib/route/link/macsec.c
@@ -41,6 +41,7 @@
 #define MACSEC_ATTR_REPLAY_PROTECT	(1 << 10)
 #define MACSEC_ATTR_VALIDATION		(1 << 11)
 #define MACSEC_ATTR_PORT		(1 << 12)
+#define MACSEC_ATTR_OFFLOAD		(1 << 13)
 
 struct macsec_info {
 	int ifindex;
@@ -52,7 +53,7 @@ struct macsec_info {
 	enum macsec_validation_type validate;
 	uint8_t encoding_sa;
 
-	uint8_t send_sci, end_station, scb, replay_protect, protect, encrypt;
+	uint8_t send_sci, end_station, scb, replay_protect, protect, encrypt, offload;
 
 	uint32_t ce_mask;
 };
@@ -74,6 +75,7 @@ static struct nla_policy macsec_policy[IFLA_MACSEC_MAX+1] = {
 	[IFLA_MACSEC_SCB] = { .type = NLA_U8 },
 	[IFLA_MACSEC_REPLAY_PROTECT] = { .type = NLA_U8 },
 	[IFLA_MACSEC_VALIDATION] = { .type = NLA_U8 },
+	[IFLA_MACSEC_OFFLOAD] = { .type = NLA_U8 },
 };
 
 /**
@@ -156,6 +158,11 @@ static int macsec_parse(struct rtnl_link *link, struct nlattr *data,
 	if (tb[IFLA_MACSEC_ENCRYPT]) {
 		info->encrypt = nla_get_u8(tb[IFLA_MACSEC_ENCRYPT]);
 		info->ce_mask |= MACSEC_ATTR_ENCRYPT;
+	}
+
+	if (tb[IFLA_MACSEC_OFFLOAD]) {
+		info->offload = nla_get_u8(tb[IFLA_MACSEC_OFFLOAD]);
+		info->ce_mask |= MACSEC_ATTR_OFFLOAD;
 	}
 
 	if (tb[IFLA_MACSEC_INC_SCI]) {
@@ -305,6 +312,9 @@ static int macsec_put_attrs(struct nl_msg *msg, struct rtnl_link *link)
 
 	if ((info->ce_mask & MACSEC_ATTR_ENCRYPT))
 		NLA_PUT_U8(msg, IFLA_MACSEC_ENCRYPT, info->encrypt);
+
+	if ((info->ce_mask & MACSEC_ATTR_OFFLOAD))
+		NLA_PUT_U8(msg, IFLA_MACSEC_OFFLOAD, info->offload);
 
 	if (info->cipher_suite != MACSEC_DEFAULT_CIPHER_ID || info->icv_len != DEFAULT_ICV_LEN) {
 		NLA_PUT_U64(msg, IFLA_MACSEC_CIPHER_SUITE, info->cipher_suite);
@@ -628,6 +638,36 @@ int rtnl_link_macsec_get_encrypt(struct rtnl_link *link, uint8_t *encrypt)
 
 	if (encrypt)
 		*encrypt = info->encrypt;
+
+	return 0;
+}
+
+int rtnl_link_macsec_set_offload(struct rtnl_link *link, uint8_t offload)
+{
+	struct macsec_info *info = link->l_info;
+
+	IS_MACSEC_LINK_ASSERT(link);
+
+	if (offload > 1)
+		return -NLE_INVAL;
+
+	info->offload = offload;
+	info->ce_mask |= MACSEC_ATTR_OFFLOAD;
+
+	return 0;
+}
+
+int rtnl_link_macsec_get_offload(struct rtnl_link *link, uint8_t *offload)
+{
+	struct macsec_info *info = link->l_info;
+
+	IS_MACSEC_LINK_ASSERT(link);
+
+	if (!(info->ce_mask & MACSEC_ATTR_OFFLOAD))
+		return -NLE_NOATTR;
+
+	if (offload)
+		*offload = info->offload;
 
 	return 0;
 }

include/linux-private/linux/if_link.h
@@ -455,6 +455,7 @@ enum {
 	IFLA_MACSEC_REPLAY_PROTECT,
 	IFLA_MACSEC_VALIDATION,
 	IFLA_MACSEC_PAD,
+	IFLA_MACSEC_OFFLOAD,
 	__IFLA_MACSEC_MAX,
 };
 

include/netlink/route/link/macsec.h
@@ -37,6 +37,9 @@ int rtnl_link_macsec_get_protect(struct rtnl_link *, uint8_t *);
 int rtnl_link_macsec_set_encrypt(struct rtnl_link *, uint8_t);
 int rtnl_link_macsec_get_encrypt(struct rtnl_link *, uint8_t *);
 
+int rtnl_link_macsec_set_offload(struct rtnl_link *, uint8_t);
+int rtnl_link_macsec_get_offload(struct rtnl_link *, uint8_t *);
+
 int rtnl_link_macsec_set_encoding_sa(struct rtnl_link *, uint8_t);
 int rtnl_link_macsec_get_encoding_sa(struct rtnl_link *, uint8_t *);
 

libnl-route-3.sym
@@ -1150,3 +1150,8 @@ global:
 	rtnl_vlan_set_vlan_id;
 	rtnl_vlan_set_vlan_prio;
 } libnl_3_4;
+
+libnl_3_6 {
+	rtnl_link_macsec_set_offload;
+	rtnl_link_macsec_get_offload;
+} libnl_3_5;

commit e69f3e0351588f37b840c1a32300f228d370e791

macsec: Drop offload capability validation check
Currently, rtnl_link_macsec_set_offload rejects any value
greater than 1 limiting the MACSEC_ATTR_OFFLOAD attribute
to the values 0 and 1 where other values are also legal.

Drop such a check since it is redundant, eventually the user
will send the netlink message to kernel, and whether the
message is valid depends on kernel.

Fixes: b6cc13d76b29 ("Supporting Hardware offload capability for MACsec")
Signed-off-by: Emeel Hakim <ehakim@nvidia.com>

lib/route/link/macsec.c
@@ -652,9 +652,6 @@ int rtnl_link_macsec_set_offload(struct rtnl_link *link, uint8_t offload)
 
 	IS_MACSEC_LINK_ASSERT(link);
 
-	if (offload > 1)
-		return -NLE_INVAL;
-
 	info->offload = offload;
 	info->ce_mask |= MACSEC_ATTR_OFFLOAD;
 
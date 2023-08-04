thom311/libnl

commit c97c8c2bfdb097ea553e302cc8dbadff2f2b9294

Add idiag-socket-details
This small program lists all sockets on the system seen by netlink and serves
as a simple example showing how to alloc an idiag msg cache and dump the
objects in it.

src/idiag-socket-details.c
@@ -1,0 +1,90 @@
+/*
+ * src/idiag-socket-details.c     List socket details
+ *
+ *	This library is free software; you can redistribute it and/or
+ *	modify it under the terms of the GNU General Public License as
+ *	published by the Free Software Foundation version 2 of the License.
+ *
+ * Copyright (c) 2013 Sassano Systems LLC <joe@sassanosystems.com>
+ */
+
+#include <netlink/cli/utils.h>
+#include <netlink/idiag/idiagnl.h>
+#include <netlink/idiag/msg.h>
+#include <linux/netlink.h>
+
+static void print_usage(void)
+{
+	printf(
+"Usage: idiag-socket-details [OPTION]\n"
+"\n"
+"Options\n"
+"     --summary		    Show socket detail summary.\n"
+"     --details             Show socket details on multiple lines.\n"
+"     --stats               Show full socket statistics.\n"
+" -h, --help                Show this help.\n"
+" -v, --version             Show versioning information.\n"
+	);
+	exit(0);
+}
+
+int main(int argc, char *argv[])
+{
+	struct nl_sock *sock;
+	struct nl_cache *idiag_cache;
+	struct nl_dump_params params = {
+		.dp_type = NL_DUMP_LINE,
+		.dp_nl_cb = NULL,
+		.dp_fd = stdout,
+	};
+	int err = 0;
+
+	sock = nl_cli_alloc_socket();
+	nl_cli_connect(sock, NETLINK_INET_DIAG);
+	for (;;) {
+		int c, optidx = 0;
+		enum {
+			ARG_SUMMARY = 257,
+			ARG_DETAILS = 258,
+			ARG_STATS = 259,
+			ARG_FAMILY,
+		};
+		static struct option long_opts[] = {
+			{ "details", 0, 0, ARG_DETAILS },
+			{ "summary", 0, 0, ARG_SUMMARY },
+			{ "stats", 0, 0, ARG_STATS},
+			{ "help", 0, 0, 'h' },
+			{ "version", 0, 0, 'v' },
+			{ 0, 0, 0, 0 }
+		};
+
+		c = getopt_long(argc, argv, "hv", long_opts, &optidx);
+		if (c == -1)
+			break;
+
+		switch (c) {
+		case '?': exit(NLE_INVAL);
+		case ARG_SUMMARY: params.dp_type = NL_DUMP_LINE; break;
+		case ARG_DETAILS: params.dp_type = NL_DUMP_DETAILS; break;
+		case ARG_STATS:   params.dp_type = NL_DUMP_STATS; break;
+		case 'h': print_usage(); break;
+		case 'v': nl_cli_print_version(); break;
+		}
+	}
+
+	if ((err = idiagnl_msg_alloc_cache(sock, AF_INET, IDIAG_SS_ALL,
+					&idiag_cache))) {
+		nl_cli_fatal(err, "Unable to allocate idiag msg cache: %s",
+				nl_geterror(err));
+	}
+
+	nl_cache_mngt_provide(idiag_cache);
+
+	nl_cache_dump_filter(idiag_cache, &params, NULL);
+
+	nl_cache_mngt_unprovide(idiag_cache);
+	nl_cache_free(idiag_cache);
+	nl_socket_free(sock);
+
+	return 0;
+}

src/.gitignore
@@ -42,3 +42,4 @@ nl-classid-lookup
 nl-pktloc-lookup
 nl-link-enslave
 nl-link-release
+idiag-socket-details

src/Makefile.am
@@ -37,7 +37,8 @@ noinst_PROGRAMS = \
 	nl-list-caches nl-list-sockets \
 	nl-util-addr \
 	nl-link-enslave \
-	nl-link-release
+	nl-link-release \
+	idiag-socket-details
 
 genl_ctrl_list_SOURCES = genl-ctrl-list.c 
 
@@ -99,3 +100,5 @@ nl_util_addr_SOURCES = nl-util-addr.c
 nl_pktloc_lookup_SOURCES = nl-pktloc-lookup.c
 
 nl_classid_lookup_SOURCES = nl-classid-lookup.c
+
+idiag_socket_details_SOURCES = idiag-socket-details.c

commit 680df1730e74e9714f16c819951cd44a19e47d83

idiag: "fix" license for "idiag-socket-details" tool
This tool had a GPL-2.0-only license text when it was added.
That is not what we require for libnl3 contributions.

Since being added, the file was only touched by me (Thomas Haller) and
Yegor Yefremov. Those are all trivial contributions.

This leaves Joe Damato, who initially contributed the file. Joe
agrees with this adjustment of the license (Thanks!!).

Note that there is also a line:

   Copyright (c) 2013 Sassano Systems LLC <joe@sassanosystems.com>

Apparently, the copyright for the contribution is also with Joe, so
there is no further problem.

Fixes: c97c8c2bfdb0 ('Add idiag-socket-details')

https://github.com/thom311/libnl/pull/354

src/idiag-socket-details.c
@@ -1,4 +1,4 @@
-/* SPDX-License-Identifier: GPL-2.0-only */
+/* SPDX-License-Identifier: LGPL-2.1-only */
 /*
  * Copyright (c) 2013 Sassano Systems LLC <joe@sassanosystems.com>
  */
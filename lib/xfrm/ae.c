thom311/libnl

commit 77bbf2270ce7c166c87933dc2d70e9619bf6b6e3

xfrm: fix an unintialized return value on memory allocation error in xfrmnl_ae_parse()
fix : err = -ENOMEM if calloc() failed

Signed-off-by: Thomas Haller <thaller@redhat.com>

lib/xfrm/ae.c
@@ -533,8 +533,10 @@ int xfrmnl_ae_parse(struct nlmsghdr *n, struct xfrmnl_ae **result)
 		struct xfrm_replay_state_esn* esn =  nla_data (tb[XFRMA_REPLAY_ESN_VAL]);
 		uint32_t len = sizeof (struct xfrmnl_replay_state_esn) +  (sizeof (uint32_t) * esn->bmp_len);
 
-		if ((ae->replay_state_esn = calloc (1, len)) == NULL)
+		if ((ae->replay_state_esn = calloc (1, len)) == NULL) {
+			err = -ENOMEM;
 			goto errout;
+		}
 		ae->replay_state_esn->oseq       =  esn->oseq;
 		ae->replay_state_esn->seq        =  esn->seq;
 		ae->replay_state_esn->oseq_hi    =  esn->oseq_hi;

commit db42483501840497dda70571cf4d51f424338503

xfrm: fix error code for NLE_ENOMEM in xfrmnl_ae_parse()
These internal error codes are probably a bad idea. However, at least be
consistent about it.

Fixes: 77bbf2270ce7 ('xfrm: fix an unintialized return value on memory allocation error in xfrmnl_ae_parse()')

lib/xfrm/ae.c
@@ -583,7 +583,7 @@ int xfrmnl_ae_parse(struct nlmsghdr *n, struct xfrmnl_ae **result)
 		uint32_t len = sizeof (struct xfrmnl_replay_state_esn) +  (sizeof (uint32_t) * esn->bmp_len);
 
 		if ((ae->replay_state_esn = calloc (1, len)) == NULL) {
-			err = -ENOMEM;
+			err = -NLE_ENOMEM;
 			goto errout;
 		}
 		ae->replay_state_esn->oseq       =  esn->oseq;
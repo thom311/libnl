thom311/libnl

commit 05bd6366387cd7824a8036de95008dc5359aa193

add support for TC action statistics
https://github.com/thom311/libnl/issues/343

Signed-off-by: Davide Caratti <dcaratti@redhat.com>

https://github.com/thom311/libnl/pull/346

lib/route/act.c
@@ -386,6 +386,13 @@ void rtnl_act_put_all(struct rtnl_act **head)
 	*head = NULL;
 }
 
+static struct nla_policy tc_act_stats_policy[TCA_STATS_MAX+1] = {
+	[TCA_STATS_BASIC]    	= { .minlen = sizeof(struct gnet_stats_basic) },
+	[TCA_STATS_QUEUE]    	= { .minlen = sizeof(struct gnet_stats_queue) },
+	[TCA_STATS_RATE_EST] 	= { .minlen = sizeof(struct gnet_stats_rate_est) },
+	[TCA_STATS_RATE_EST64] 	= { .minlen = sizeof(struct gnet_stats_rate_est64) },
+};
+
 int rtnl_act_parse(struct rtnl_act **head, struct nlattr *tb)
 {
 	struct rtnl_act *act;
@@ -432,6 +439,45 @@ int rtnl_act_parse(struct rtnl_act **head, struct nlattr *tb)
 				goto err_free;
 			}
 			tc->ce_mask |= TCA_ATTR_OPTS;
+		}
+
+		if (tb2[TCA_ACT_STATS]) {
+			struct nlattr *tb3[TCA_STATS_MAX + 1];
+
+			err = nla_parse_nested(tb3, TCA_STATS_MAX, tb2[TCA_ACT_STATS],
+					       tc_act_stats_policy);
+			if (err < 0)
+				return err;
+
+			if (tb3[TCA_STATS_BASIC]) {
+				struct gnet_stats_basic bs;
+
+				memcpy(&bs, nla_data(tb3[TCA_STATS_BASIC]),
+				       sizeof(bs));
+				tc->tc_stats[RTNL_TC_BYTES] = bs.bytes;
+				tc->tc_stats[RTNL_TC_PACKETS] = bs.packets;
+			}
+			if (tb3[TCA_STATS_RATE_EST64]) {
+				struct gnet_stats_rate_est64 re;
+
+				memcpy(&re, nla_data(tb3[TCA_STATS_RATE_EST64]),
+				       sizeof(re));
+				tc->tc_stats[RTNL_TC_RATE_BPS] = re.bps;
+				tc->tc_stats[RTNL_TC_RATE_PPS] = re.pps;
+			} else if (tb3[TCA_STATS_RATE_EST]) {
+				struct gnet_stats_rate_est *re;
+
+				re = nla_data(tb3[TCA_STATS_RATE_EST]);
+				tc->tc_stats[RTNL_TC_RATE_BPS] = re->bps;
+				tc->tc_stats[RTNL_TC_RATE_PPS] = re->pps;
+			}
+			if (tb3[TCA_STATS_QUEUE]) {
+				struct gnet_stats_queue *q;
+
+				q = nla_data(tb3[TCA_STATS_QUEUE]);
+				tc->tc_stats[RTNL_TC_DROPS] = q->drops;
+				tc->tc_stats[RTNL_TC_OVERLIMITS] = q->overlimits;
+			}
 		}
 
 		ops = rtnl_tc_get_ops(tc);

commit a70f789a79d734979af8610dadb3e2ee42e6b6ee

route: fix memleak in rtnl_act_parse()
Coverity warns:

    Error: RESOURCE_LEAK (CWE-772): [#def2]
    libnl-3.8.0/lib/route/act.c:421: alloc_fn: Storage is returned from allocation function "rtnl_act_alloc".
    libnl-3.8.0/lib/route/act.c:421: var_assign: Assigning: "act" = storage returned from "rtnl_act_alloc()".
    libnl-3.8.0/lib/route/act.c:426: var_assign: Assigning: "tc" = "act".
    libnl-3.8.0/lib/route/act.c:438: noescape: Resource "tc" is not freed or pointed-to in "rtnl_tc_set_kind".
    libnl-3.8.0/lib/route/act.c:455: leaked_storage: Variable "tc" going out of scope leaks the storage it points to.
    libnl-3.8.0/lib/route/act.c:455: leaked_storage: Variable "act" going out of scope leaks the storage it points to.
    #  453|   					       tc_act_stats_policy);
    #  454|   			if (err < 0)
    #  455|-> 				return err;
    #  456|
    #  457|   			if (tb3[TCA_STATS_BASIC]) {

Fixes: 05bd6366387c ('add support for TC action statistics')

lib/route/act.c
@@ -452,7 +452,7 @@ int rtnl_act_parse(struct rtnl_act **head, struct nlattr *tb)
 			err = nla_parse_nested(tb3, TCA_STATS_MAX, tb2[TCA_ACT_STATS],
 					       tc_act_stats_policy);
 			if (err < 0)
-				return err;
+				goto err_free;
 
 			if (tb3[TCA_STATS_BASIC]) {
 				struct gnet_stats_basic bs;
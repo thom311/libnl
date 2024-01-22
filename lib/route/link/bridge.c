36d178894fdd16a8144559e526419d255a87ca99
Cordell O'Leary <cordell.oleary@alliedtelesis.co.nz>
2024-01-23 11:58:59 +1300

    tests: add test for bridge vlan attributes.

--- lib/route/link/bridge.c
+++ lib/route/link/bridge.c
@@ -23,6 +23,7 @@
 #include "nl-route.h"
 #include "link-api.h"
 #include "nl-priv-dynamic-core/nl-core.h"
+#include "nl-priv-static-route/nl-priv-static-route.h"
 
 #define VLAN_VID_MASK           0x0fff /* VLAN Identifier */
 
@@ -257,7 +258,7 @@ static int bridge_parse_af_full(struct rtnl_link *link, struct nlattr *attr_full
 	return 0;
 }
 
-static int bridge_fill_vlan_info(struct nl_msg *msg, struct rtnl_link_bridge_vlan * vlan_info) 
+int _nl_bridge_fill_vlan_info(struct nl_msg *msg, struct rtnl_link_bridge_vlan * vlan_info) 
 {
 	struct bridge_vlan_info vinfo;
 	int i = -1, j, k;
@@ -397,7 +398,7 @@ static int bridge_fill_af(struct rtnl_link *link, struct nl_msg *msg,
 
 	if (bd->ce_mask & BRIDGE_ATTR_PORT_VLAN)
 	{
-		if(bridge_fill_vlan_info(msg,&bd->vlan_info)){
+		if(_nl_bridge_fill_vlan_info(msg,&bd->vlan_info)){
 			goto nla_put_failure;
 		}
 	}

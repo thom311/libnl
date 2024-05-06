thom311/libnl

commit 1dbdc30a6b9f96355aadf69899f124480aa4d374

cache: allow to allocate cache manager with custom refill socket
Cache managers use two sockets: one for cache refill operation,
and another one for notifications.

In order to simulate NETLINK events by reading data from files,
we need to be able to overwrite callbacks for both sockets.

This new function allows us to set up refill socket any way we want.
It does have requirement that the refill socket be blocking.

include/netlink/cache.h
@@ -140,10 +140,15 @@ extern struct nl_cache *	__nl_cache_mngt_require(const char *);
 
 struct nl_cache_mngr;
 
-#define NL_AUTO_PROVIDE		1
-#define NL_ALLOCATED_SOCK	2  /* For internal use only, do not use */
+#define NL_AUTO_PROVIDE		    1
+#define NL_ALLOCATED_SOCK	    2  /* For internal use only, do not use */
+#define NL_ALLOCATED_SYNC_SOCK	4  /* For internal use only, do not use */
 
 extern int			nl_cache_mngr_alloc(struct nl_sock *,
+						    int, int,
+						    struct nl_cache_mngr **);
+extern int			nl_cache_mngr_alloc_ex(struct nl_sock *,
+							struct nl_sock *,
 						    int, int,
 						    struct nl_cache_mngr **);
 extern int			nl_cache_mngr_add(struct nl_cache_mngr *,

lib/cache_mngr.c
@@ -149,16 +149,45 @@ found:
 int nl_cache_mngr_alloc(struct nl_sock *sk, int protocol, int flags,
 			struct nl_cache_mngr **result)
 {
+	return nl_cache_mngr_alloc_ex(sk, NULL, protocol, flags, result);
+}
+
+/**
+ * Allocate new cache manager, with custom callback on refill socket
+ * @arg sk		Netlink socket or NULL to auto allocate
+ * @arg sync_sk		Blocking Netlink socket for cache refills
+ * @arg protocol	Netlink protocol this manager is used for
+ * @arg flags		Flags (\c NL_AUTO_PROVIDE)
+ * @arg result		Result pointer
+ *
+ * Same as \f nl_cache_mngr_alloc, but sets custom refill socket
+ * Note: ownership of the sync_sk passes to the cache manager
+ */
+int nl_cache_mngr_alloc_ex(struct nl_sock *sk, struct nl_sock *sync_sk, int protocol, int flags,
+			struct nl_cache_mngr **result)
+{
 	struct nl_cache_mngr *mngr;
 	int err = -NLE_NOMEM;
 
 	/* Catch abuse of flags */
-	if (flags & NL_ALLOCATED_SOCK)
+	if (flags & (NL_ALLOCATED_SOCK|NL_ALLOCATED_SYNC_SOCK))
 		BUG();
 
 	mngr = calloc(1, sizeof(*mngr));
-	if (!mngr)
+	if (!mngr) {
 		return -NLE_NOMEM;
+	}
+
+	if(!sync_sk) {
+		if (!(sync_sk = nl_socket_alloc()))
+			goto errout;
+
+		flags |= NL_ALLOCATED_SOCK;
+	}
+	/* have to assign before calling nl_connect, so that it gets freed in case
+	 * of an nl_socket_allock error for sk
+	 */
+	mngr->cm_sync_sock = sync_sk;
 
 	if (!sk) {
 		if (!(sk = nl_socket_alloc()))
@@ -176,6 +205,10 @@ int nl_cache_mngr_alloc(struct nl_sock *sk, int protocol, int flags,
 	if (!mngr->cm_assocs)
 		goto errout;
 
+	if ((err = nl_connect(sync_sk, protocol)) < 0) {
+		goto errout;
+	}
+
 	/* Required to receive async event notifications */
 	nl_socket_disable_seq_check(mngr->cm_sock);
 
@@ -185,14 +218,6 @@ int nl_cache_mngr_alloc(struct nl_sock *sk, int protocol, int flags,
 	if ((err = nl_socket_set_nonblocking(mngr->cm_sock)) < 0)
 		goto errout;
 
-	/* Create and allocate socket for sync cache fills */
-	mngr->cm_sync_sock = nl_socket_alloc();
-	if (!mngr->cm_sync_sock) {
-		err = -NLE_NOMEM;
-		goto errout;
-	}
-	if ((err = nl_connect(mngr->cm_sync_sock, protocol)) < 0)
-		goto errout_free_sync_sock;
 
 	NL_DBG(1, "Allocated cache manager %p, protocol %d, %d caches\n",
 	       mngr, protocol, mngr->cm_nassocs);
@@ -200,8 +225,6 @@ int nl_cache_mngr_alloc(struct nl_sock *sk, int protocol, int flags,
 	*result = mngr;
 	return 0;
 
-errout_free_sync_sock:
-	nl_socket_free(mngr->cm_sync_sock);
 errout:
 	nl_cache_mngr_free(mngr);
 	return err;
@@ -624,8 +647,10 @@ void nl_cache_mngr_free(struct nl_cache_mngr *mngr)
 
 	if (mngr->cm_sync_sock) {
 		nl_close(mngr->cm_sync_sock);
-		nl_socket_free(mngr->cm_sync_sock);
 	}
+
+	if (mngr->cm_flags & NL_ALLOCATED_SYNC_SOCK)
+		nl_socket_free(mngr->cm_sync_sock);
 
 	if (mngr->cm_flags & NL_ALLOCATED_SOCK)
 		nl_socket_free(mngr->cm_sock);

libnl-3.sym
@@ -372,3 +372,8 @@ global:
 
 libnl_3_6 {
 } libnl_3_5;
+
+libnl_3_10 {
+global:
+	nl_cache_mngr_alloc_ex;
+} libnl_3_6;

commit 32cb9f392c77aa4b8ea0a05f6876021a4816814d

cache: cleanup nl_cache_mngr_alloc_ex()
- mngr->cm_flags must be set together when assigning the socket.
  Otherwise, a `goto errout` in the middle will cause a leak.

- normalize the flags variable to not contain unexpected values.

- NL_ALLOCATED_SYNC_SOCK is private API. No need to expose that to
  public headers.

Fixes: 1dbdc30a6b9f ('cache: allow to allocate cache manager with custom refill socket')

include/netlink/cache.h
@@ -142,15 +142,14 @@ struct nl_cache_mngr;
 
 #define NL_AUTO_PROVIDE		    1
 #define NL_ALLOCATED_SOCK	    2  /* For internal use only, do not use */
-#define NL_ALLOCATED_SYNC_SOCK	4  /* For internal use only, do not use */
 
 extern int			nl_cache_mngr_alloc(struct nl_sock *,
 						    int, int,
 						    struct nl_cache_mngr **);
 extern int			nl_cache_mngr_alloc_ex(struct nl_sock *,
-							struct nl_sock *,
-						    int, int,
-						    struct nl_cache_mngr **);
+						       struct nl_sock *,
+						       int, int,
+						       struct nl_cache_mngr **);
 extern int			nl_cache_mngr_add(struct nl_cache_mngr *,
 						  const char *,
 						  change_func_t,

lib/cache_mngr.c
@@ -37,6 +37,8 @@
 #include "nl-priv-dynamic-core/cache-api.h"
 #include "nl-aux-core/nl-core.h"
 
+#define NL_ALLOCATED_SYNC_SOCK 4
+
 /** @cond SKIP */
 struct nl_cache_mngr
 {
@@ -167,45 +169,43 @@ int nl_cache_mngr_alloc_ex(struct nl_sock *sk, struct nl_sock *sync_sk, int prot
 			struct nl_cache_mngr **result)
 {
 	struct nl_cache_mngr *mngr;
-	int err = -NLE_NOMEM;
+	int err;
 
 	/* Catch abuse of flags */
-	if (flags & (NL_ALLOCATED_SOCK|NL_ALLOCATED_SYNC_SOCK))
+	if (flags & NL_ALLOCATED_SOCK)
 		BUG();
+	flags = flags & NL_AUTO_PROVIDE;
 
 	mngr = calloc(1, sizeof(*mngr));
-	if (!mngr) {
+	if (!mngr)
 		return -NLE_NOMEM;
-	}
 
-	if(!sync_sk) {
-		if (!(sync_sk = nl_socket_alloc()))
-			goto errout;
-
-		flags |= NL_ALLOCATED_SOCK;
-	}
-	/* have to assign before calling nl_connect, so that it gets freed in case
-	 * of an nl_socket_allock error for sk
-	 */
-	mngr->cm_sync_sock = sync_sk;
+	mngr->cm_flags = flags;
 
 	if (!sk) {
-		if (!(sk = nl_socket_alloc()))
+		if (!(sk = nl_socket_alloc())) {
+			err = -NLE_NOMEM;
 			goto errout;
-
-		flags |= NL_ALLOCATED_SOCK;
+		}
+		mngr->cm_flags |= NL_ALLOCATED_SOCK;
 	}
-
 	mngr->cm_sock = sk;
+
+	if(!sync_sk) {
+		if (!(sync_sk = nl_socket_alloc())) {
+			err = -NLE_NOMEM;
+			goto errout;
+		}
+		mngr->cm_flags |= NL_ALLOCATED_SYNC_SOCK;
+	}
+	mngr->cm_sync_sock = sync_sk;
+
 	mngr->cm_nassocs = NASSOC_INIT;
 	mngr->cm_protocol = protocol;
-	mngr->cm_flags = flags;
 	mngr->cm_assocs = calloc(mngr->cm_nassocs,
 				 sizeof(struct nl_cache_assoc));
-	if (!mngr->cm_assocs)
-		goto errout;
-
-	if ((err = nl_connect(sync_sk, protocol)) < 0) {
+	if (!mngr->cm_assocs) {
+		err = -NLE_NOMEM;
 		goto errout;
 	}
 
@@ -218,6 +218,8 @@ int nl_cache_mngr_alloc_ex(struct nl_sock *sk, struct nl_sock *sync_sk, int prot
 	if ((err = nl_socket_set_nonblocking(mngr->cm_sock)) < 0)
 		goto errout;
 
+	if ((err = nl_connect(mngr->cm_sync_sock, protocol)) < 0)
+		goto errout;
 
 	NL_DBG(1, "Allocated cache manager %p, protocol %d, %d caches\n",
 	       mngr, protocol, mngr->cm_nassocs);
@@ -645,15 +647,14 @@ void nl_cache_mngr_free(struct nl_cache_mngr *mngr)
 	if (mngr->cm_sock)
 		nl_close(mngr->cm_sock);
 
-	if (mngr->cm_sync_sock) {
+	if (mngr->cm_sync_sock)
 		nl_close(mngr->cm_sync_sock);
-	}
-
-	if (mngr->cm_flags & NL_ALLOCATED_SYNC_SOCK)
-		nl_socket_free(mngr->cm_sync_sock);
 
 	if (mngr->cm_flags & NL_ALLOCATED_SOCK)
 		nl_socket_free(mngr->cm_sock);
+
+	if (mngr->cm_flags & NL_ALLOCATED_SYNC_SOCK)
+		nl_socket_free(mngr->cm_sync_sock);
 
 	for (i = 0; i < mngr->cm_nassocs; i++) {
 		if (mngr->cm_assocs[i].ca_cache) {
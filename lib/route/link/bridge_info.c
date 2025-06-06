thom311/libnl

commit 7391a38e3d5edc5aac11c9d2fa0bf5ca67655bdb

bridge: Add support for link_info of a bridge
And functions to access some new bridge attributes.

Signed-off-by: Langer Thomas <tlanger@maxlinear.com>

Co-Authored-By: Kacper Ludwinski <kludwinski@maxlinear.com>

lib/route/link/bridge_info.c
@@ -1,0 +1,119 @@
+/*
+ * lib/route/link/bridge_info.c	bridge info support
+ *
+ *	This library is free software; you can redistribute it and/or
+ *	modify it under the terms of the GNU Lesser General Public
+ *	License as published by the Free Software Foundation version 2.1
+ *	of the License.
+ *
+ * Copyright (c) 2022 MaxLinear, Inc.
+ */
+
+/**
+ * @ingroup link
+ * @defgroup bridge Bridging
+ *
+ * @details
+ * @{
+ */
+
+#include <netlink-private/netlink.h>
+#include <netlink-private/route/link/api.h>
+
+struct bridge_info
+{
+	uint32_t		ce_mask; /* to support attr macros */
+};
+
+static struct nla_policy bi_attrs_policy[IFLA_BR_MAX+1] = {
+
+};
+
+static inline struct bridge_info *bridge_info(struct rtnl_link *link)
+{
+	return link->l_info;
+}
+
+static int bridge_info_alloc(struct rtnl_link *link)
+{
+	struct bridge_info *bi;
+
+	if (link->l_info)
+		memset(link->l_info, 0, sizeof(*bi));
+	else {
+		bi = calloc(1, sizeof(*bi));
+		if (!bi)
+			return -NLE_NOMEM;
+
+		link->l_info = bi;
+	}
+
+	return 0;
+}
+
+static int bridge_info_parse(struct rtnl_link *link, struct nlattr *data,
+			     struct nlattr *xstats)
+{
+	struct nlattr *tb[IFLA_BR_MAX+1];
+	struct bridge_info *bi;
+	int err = 0;
+
+	NL_DBG(3, "Parsing Bridge link info\n");
+
+	if ((err = nla_parse_nested(tb, IFLA_BR_MAX, data, bi_attrs_policy)) < 0)
+		goto errout;
+
+	if ((err = bridge_info_alloc(link)) < 0)
+		goto errout;
+
+	bi = link->l_info;
+
+errout:
+	return err;
+}
+
+static int bridge_info_put_attrs(struct nl_msg *msg, struct rtnl_link *link)
+{
+	struct bridge_info *bi = link->l_info;
+	struct nlattr *data;
+
+	data = nla_nest_start(msg, IFLA_INFO_DATA);
+	if (!data)
+		return -NLE_MSGSIZE;
+
+	nla_nest_end(msg, data);
+	return 0;
+}
+
+static void bridge_info_free(struct rtnl_link *link)
+{
+	free(link->l_info);
+	link->l_info = NULL;
+}
+
+static struct rtnl_link_info_ops bridge_info_ops = {
+	.io_name		= "bridge",
+	.io_alloc		= bridge_info_alloc,
+	.io_parse		= bridge_info_parse,
+	.io_put_attrs		= bridge_info_put_attrs,
+	.io_free		= bridge_info_free,
+};
+
+#define IS_BRIDGE_INFO_ASSERT(link) \
+	do { \
+		if ((link)->l_info_ops != &bridge_info_ops) { \
+			APPBUG("Link is not a bridge link. Set type \"bridge\" first."); \
+		} \
+	} while(0)
+
+static void __init bridge_info_init(void)
+{
+	rtnl_link_register_info(&bridge_info_ops);
+}
+
+static void __exit bridge_info_exit(void)
+{
+	rtnl_link_unregister_info(&bridge_info_ops);
+}
+
+/** @} */

Makefile.am
@@ -404,6 +404,7 @@ lib_libnl_route_3_la_SOURCES = \
 	lib/route/link.c \
 	lib/route/link/api.c \
 	lib/route/link/bonding.c \
+	lib/route/link/bridge_info.c \
 	lib/route/link/bridge.c \
 	lib/route/link/can.c \
 	lib/route/link/dummy.c \

commit 615a8c8b8aeaf10b78fdf38e25e8f487d2d23d96

bridge_info: implement missing rtnl_link_info_ops::io_compare()
Without rtnl_link_info_ops::io_compare() changes in link info are not
picked up by the cache and no notificatons are sent out as
rtnl_link_info_data_compare() returns 0 when the callback is missing.

Fix this by implementing the missing io_compare() for bridge_info.

This allows e.g. changes in ageing time to be seen.

Fixes: 7391a38e3d5e ("bridge: Add support for link_info of a bridge")
Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

lib/route/link/bridge_info.c
@@ -261,6 +261,48 @@ static void bridge_info_free(struct rtnl_link *link)
 	_nl_clear_free(&link->l_info);
 }
 
+static int bridge_info_compare(struct rtnl_link *link_a,
+			       struct rtnl_link *link_b, int flags)
+{
+	struct bridge_info *a = link_a->l_info;
+	struct bridge_info *b = link_b->l_info;
+	uint32_t attrs = flags & LOOSE_COMPARISON ? b->ce_mask : ~0u;
+	int diff = 0;
+
+#define _DIFF(ATTR, EXPR) ATTR_DIFF(attrs, ATTR, a, b, EXPR)
+	diff |= _DIFF(BRIDGE_ATTR_VLAN_FILTERING,
+		      a->b_vlan_filtering != b->b_vlan_filtering);
+	diff |= _DIFF(BRIDGE_ATTR_VLAN_PROTOCOL,
+		      a->b_vlan_protocol != b->b_vlan_protocol);
+	diff |= _DIFF(BRIDGE_ATTR_VLAN_STATS_ENABLED,
+		      a->b_vlan_stats_enabled != b->b_vlan_stats_enabled);
+	diff |= _DIFF(BRIDGE_ATTR_AGEING_TIME,
+		      a->b_ageing_time != b->b_ageing_time);
+	diff |= _DIFF(BRIDGE_ATTR_VLAN_DEFAULT_PVID,
+		      a->b_vlan_default_pvid != b->b_vlan_default_pvid);
+	diff |= _DIFF(BRIDGE_ATTR_NF_CALL_IPTABLES,
+		      a->b_nf_call_iptables != b->b_nf_call_iptables);
+	diff |= _DIFF(BRIDGE_ATTR_NF_CALL_IP6TABLES,
+		      a->b_nf_call_ip6tables != b->b_nf_call_ip6tables);
+	diff |= _DIFF(BRIDGE_ATTR_NF_CALL_ARPTABLES,
+		      a->b_nf_call_arptables != b->b_nf_call_arptables);
+	diff |= _DIFF(BRIDGE_ATTR_STP_STATE, a->b_stp_state != b->b_stp_state);
+	diff |= _DIFF(BRIDGE_ATTR_MCAST_ROUTER,
+		      a->b_mcast_router != b->b_mcast_router);
+	diff |= _DIFF(BRIDGE_ATTR_MCAST_SNOOPING,
+		      a->b_mcast_snooping != b->b_mcast_snooping);
+	if (flags & LOOSE_COMPARISON)
+		diff |= _DIFF(BRIDGE_ATTR_BOOLOPT,
+			      (a->b_boolopts.optval ^ b->b_boolopts.optval) &
+				      b->b_boolopts.optmask);
+	else
+		diff |= _DIFF(BRIDGE_ATTR_BOOLOPT,
+			      a->b_boolopts.optval != b->b_boolopts.optval);
+#undef _DIFF
+
+	return diff;
+}
+
 static struct rtnl_link_info_ops bridge_info_ops = {
 	.io_name = "bridge",
 	.io_alloc = bridge_info_alloc,
@@ -268,6 +310,7 @@ static struct rtnl_link_info_ops bridge_info_ops = {
 	.io_parse = bridge_info_parse,
 	.io_put_attrs = bridge_info_put_attrs,
 	.io_free = bridge_info_free,
+	.io_compare = bridge_info_compare,
 };
 
 #define IS_BRIDGE_INFO_ASSERT(link)                                                      \
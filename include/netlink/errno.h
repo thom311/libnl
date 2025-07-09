45449e0bc6932c65be19674f4bc9db9ec99fa369
Christoph Paasch <cpaasch@openai.com>
2025-07-08 20:28:34 -0700

    Add EHOSTUNREACH to nl_syserr2nlerr
    For improved error-output.
    
    Signed-off-by: Christoph Paasch <cpaasch@openai.com>
    
    https://github.com/thom311/libnl/pull/433

--- include/netlink/errno.h
+++ include/netlink/errno.h
@@ -45,8 +45,9 @@ extern "C" {
 #define NLE_IMMUTABLE		32
 #define NLE_DUMP_INTR		33
 #define NLE_ATTRSIZE		34
+#define NLE_HOSTUNREACH		35
 
-#define NLE_MAX		NLE_ATTRSIZE
+#define NLE_MAX		NLE_HOSTUNREACH
 
 extern const char *	nl_geterror(int);
 extern void		nl_perror(int, const char *);
